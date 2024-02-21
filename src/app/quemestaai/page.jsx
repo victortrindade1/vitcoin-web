5ea62430841a80f697c8534cd78838417fc5a8db
Victor Trindade <victortrindade@gmail.com>
2024-02-20 22:49:14 -0300

    stuff

--- src/app/quemestaai/page.jsx
+++ src/app/quemestaai/page.jsx
@@ -15,7 +15,7 @@ import {
   PassInput,
 } from "./styles";
 
-function entre() {
+function quemestaai() {
   const [contadorClicks, setContadorClicks] = useState(0);
   const [mostrarDurinDoor, setMostrarDurinDoor] = useState(false);
   const [mostrarTitle, setMostrarTitle] = useState(false);
@@ -59,4 +59,4 @@ function entre() {
   );
 }
 
-export default entre;
+export default quemestaai;
